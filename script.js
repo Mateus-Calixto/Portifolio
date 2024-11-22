abd5ea4ba63cd971fcae97fbc2f85cadb169fbed
Mateus Vinicius Calixto <mc3230855@gmail.com>
2024-11-22 16:50:22 -0300

    Finalização da estrutura do projeto
    acabamento do css e do html: proximo passo completar os campos com dados reais e os linkes tambem

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function menuShow() {
         document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
     }
 }
+
+
+// Seleciona o botão
+const backToTopButton = document.getElementById("voltar-topo");
+
+// Mostra o botão ao rolar a página
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    backToTopButton.classList.add("visible");
+  } else {
+    backToTopButton.classList.remove("visible");
+  }
+});
+
+// Volta ao topo quando o botão é clicado
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
