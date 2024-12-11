582f142ab40d9fcc5c5b0a06170adccf3a9eb5ad
Mateus Vinicius Calixto <mc3230855@gmail.com>
2024-12-11 17:36:50 -0300

    Preenchendo as habilidades

--- script.js
+++ script.js
@@ -26,3 +26,20 @@ window.addEventListener("scroll", () => {
 backToTopButton.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+
+/*Modal*/
+
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
